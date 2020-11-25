6aa154676f28755d19daa6c35937b7a8254160e4
git <C0deV#>
2020-11-25 11:08:21 -0700

    testt

--- test.R
+++ test.R
@@ -1,5 +1,11 @@
 library(biomaRt)
 library(caret)
+library(caTools)
 
+print(iris$Sepal.Length)
+sample.split(iris,SplitRatio = 0.7)->s
+subset(iris, s==TRUE)->train_data
+subset(iris, s==FALSE)->test_data
 
-afasdfafa 
+lm(train_data$Sepal.Length~., train_data)->model1
+summary(model1)
